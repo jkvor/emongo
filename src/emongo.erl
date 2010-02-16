be228bc68d88a47cafcaae3fa8520eb3c5433a7b
Dmitry Belyaev <rumata@yandex-team.ru>
2010-02-16 13:41:02 +0300

    Use transform_selector on Selector in delete/3 so that the selector works. http://github.com/JacobVorreuter/emongo/commit/2e34698549a191c543d92c7621e14f0b2d83e7c2

--- src/emongo.erl
+++ src/emongo.erl
@@ -199,7 +199,7 @@ delete(PoolId, Collection) ->
 
 delete(PoolId, Collection, Selector) ->
 	{Pid, Pool} = gen_server:call(?MODULE, {pid, PoolId}, infinity),
-	Packet = emongo_packet:delete(Pool#pool.database, Collection, Pool#pool.req_id, Selector),
+	Packet = emongo_packet:delete(Pool#pool.database, Collection, Pool#pool.req_id, transform_selector(Selector)),
 	emongo_server:send(Pid, Pool#pool.req_id, Packet).
 
 %%------------------------------------------------------------------------------
